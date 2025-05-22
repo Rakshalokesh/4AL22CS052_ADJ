71e7c12739d3c635e084e3c6801fea640e3372b1
Raksha <144310981+Rakshalokesh@users.noreply.github.com>
2025-05-22 21:40:35 +0530

    Update pg4a.java

--- 4a.jframe/pg4a.java
+++ 4a.jframe/pg4a.java
@@ -1,3 +1,7 @@
+//4a. Basic hello program of Swing displaying the message Hello! VI C , Welcome to Swing
+//Programming in Blue color plain font with font size of 32 using Jframe and Jlabel
+
+
 package github1;
 import java.awt.Color;
 import java.awt.Font;
