cbaf7375011706db8924d31332ad002c453a2310
Raksha <144310981+Rakshalokesh@users.noreply.github.com>
2025-05-22 11:08:47 +0530

    Update linked1c2.java

--- 1c.Linkedlist/linked1c2.java
+++ 1c.Linkedlist/linked1c2.java
@@ -1,3 +1,7 @@
+//2. Write a Java program to iterate a linked list in reverse order (using
+//objlist.descendingIterator())
+
+
 package github1;
 import java.util.LinkedList;
 import java.util.Iterator;
