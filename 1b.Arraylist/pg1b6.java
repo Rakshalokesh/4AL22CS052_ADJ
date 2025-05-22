1f324b4bfede43669aaf32b7961db6a1227cb772
Raksha <144310981+Rakshalokesh@users.noreply.github.com>
2025-05-22 10:40:40 +0530

    Update pg1b6.java

--- 1b.Arraylist/pg1b6.java
+++ 1b.Arraylist/pg1b6.java
@@ -1,3 +1,7 @@
+//6. Write a java program for getting different colors through ArrayList interface and delete nth
+//element from the ArrayList object by using remove by index
+
+
 package github1;
 import java.util.ArrayList;
 import java.util.Scanner;
